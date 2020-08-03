d040f1c2d172fc7a0a06c00de07596929b06db8a
marcin.sciesinski <marcin.sciesinski>
2020-08-03 13:14:11 -0700

    Added proper tabs

---
+++ desktop/src/main/java/com/gempukku/libgdx/graph/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gempukku.libgdx.graph.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gempukku.libgdx.graph.LibgdxGraph;
+
+public class DesktopLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibgdxGraph(), config);
+	}
+}
