c445141cee6a7c31921bf4157e437e637ae5819b
MarcinSc <marcins78@gmail.com>
2020-08-19 12:46:12 +0700

    Episode 4

--- libgdx-graph-test/src/com/gempukku/libgdx/graph/test/TestDesktopLauncher.java
+++ libgdx-graph-test/src/com/gempukku/libgdx/graph/test/TestDesktopLauncher.java
@@ -2,13 +2,13 @@ package com.gempukku.libgdx.graph.test;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.gempukku.libgdx.graph.test.episodes.Episode3LibgdxGraphTestApplication;
+import com.gempukku.libgdx.graph.test.episodes.Episode4LibgdxGraphTestApplication;
 
 public class TestDesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 1440;
         config.height = 810;
-        new LwjglApplication(new Episode3LibgdxGraphTestApplication(), config);
+        new LwjglApplication(new Episode4LibgdxGraphTestApplication(), config);
     }
 }
